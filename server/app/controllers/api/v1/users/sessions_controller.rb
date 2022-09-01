8a3e01ce4e44dc5aa9580bd7cd9188671ddc9ae7
Vittor Custodio <vittorvc@gmail.com>
2022-09-01 09:40:54 -0300

    chore: Adding users sessions controller

---
+++ server/app/controllers/api/v1/users/sessions_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Api::V1::Users::SessionsController < ::Devise::SessionsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: {
+      status: {code: 200, message: 'Logged in sucessfully.'},
+      data: resource
+    }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    if current_user
+      render json: {
+        status: {code: 200, message: "Logged out successfully"},
+      }, status: :ok
+    else
+      render json: {
+        status: {code: 401, message: "Couldn't find an active session."},
+      }, status: :unauthorized
+    end
+  end
+end
