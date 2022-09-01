c029650d5b2cf6eb499c4a1e208f274954163dca
Vittor Custodio <vittorvc@gmail.com>
2022-09-01 09:50:01 -0300

    feat: Adding routes and setting up jwt for auth

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, skip: :all
+  devise_scope :user do
+    post "/api/v1/signup", to: "api/v1/users/registrations#create", as: 'user_registration'
+    post "/api/v1/login", to: "api/v1/users/sessions#create", as: 'user_session'
+    delete "/api/v1/logout", to: "api/v1/users/sessions#destroy", as: 'destroy_user_session'
+  end
 end
