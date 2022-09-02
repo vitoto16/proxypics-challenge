20332356cd1cb0418a79ac8fcb65480988d88104
Vittor Custodio <vittorvc@gmail.com>
2022-09-02 14:27:45 -0300

    feat: Setting up base controller
    - Handling responses in json
    - Authenticating requester user type

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+    include ActionController::MimeResponds
+    respond_to :json
+
+    def authenticate_requester!
+        authenticate_user!
+        if !current_user.is_a? Requester
+            return render json: { message: "User type not authorized to perform this action" }, status: :unauthorized
+        end
+    end
 end
