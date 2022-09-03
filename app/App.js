6fea28328a3c65f3ba23d19e0ecde71211a747a3
Vittor Custodio <vittorvc@gmail.com>
2022-09-03 18:05:45 -0300

    feat: Connecting redux store

--- app/App.js
+++ app/App.js
@@ -7,13 +7,21 @@
  */
 
 import React from 'react';
+import {createStore, applyMiddleware} from 'redux';
 import {Text, View} from 'react-native';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import reducers from './src/reducers';
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const App = () => {
   return (
-    <View>
-      <Text>Initial App!</Text>
-    </View>
+    <Provider store={store}>
+      <View>
+        <Text>Initial App!</Text>
+      </View>
+    </Provider>
   );
 };
 
